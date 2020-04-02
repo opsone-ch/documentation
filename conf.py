17059deec746953e9e3fffd03e231dcb7c19a308
Andri Steiner <andri@opsone.ch>
2020-04-02 19:51:44 +0200

    opensource/puppet-modules#739 fix branch name in GitLab configuration

--- conf.py
+++ conf.py
@@ -115,7 +115,7 @@ html_context = {
     "display_gitlab": True,
     "gitlab_user": "opsone_ch",
     "gitlab_repo": 'documentation',
-    "gitlab_version": "6-0-stable",
+    "gitlab_version": "managed-server-6",
     "conf_py_path": "/",
     "source_suffix": '.rst',
     "theme_vcs_pageview_mode": "blob",
